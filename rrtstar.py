51e4a82c35d4598176fcbf95783205f6743aadc0
Basano <81038341+Basano@users.noreply.github.com>
2021-04-02 11:25:10 +0200

    Removal of import of RRT script

--- rrtstar.py
+++ rrtstar.py
@@ -1,9 +1,7 @@
-"""
-Path planning Sample Code with RRT*
-author: Atsushi Sakai(@Atsushi_twi)
-"""
 
 import math
+import numpy as np
+import random
 import os
 import sys
 
@@ -11,22 +9,26 @@ import matplotlib.pyplot as plt
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../RRT/")
 
-try:
-    from rrt import RRT
-except ImportError:
-    raise
+# try:
+#     from rrt import RRT
+# except ImportError:
+#     raise
 
 show_animation = False
 
 
-class RRTStar(RRT):
+class RRTStar:
     """
     Class for RRT Star planning
     """
 
-    class Node(RRT.Node):
+    class Node:
         def __init__(self, x, y):
-            super().__init__(x, y)
+            self.x = x
+            self.y = y
+            self.path_x = []
+            self.path_y = []
+            self.parent = None
             self.cost = 0.0
 
     def __init__(self,
@@ -37,7 +39,7 @@ class RRTStar(RRT):
                  expand_dis=5,
                  path_resolution=1.0,
                  goal_sample_rate=5,
-                 max_iter=100,
+                 max_iter=1000,
                  connect_circle_dist=50.0,
                  search_until_max_iter=True):
         """
@@ -47,8 +49,16 @@ class RRTStar(RRT):
         obstacleList:obstacle Positions [[x,y,size],...]
         randArea:Random Sampling Area [min,max]
         """
-        super().__init__(start, goal, obstacle_list, rand_area, expand_dis,
-                         path_resolution, goal_sample_rate, max_iter)
+        self.start = self.Node(start[0], start[1])
+        self.end = self.Node(goal[0], goal[1])
+        self.min_rand = rand_area[0]
+        self.max_rand = rand_area[1]
+        self.expand_dis = expand_dis
+        self.path_resolution = path_resolution
+        self.goal_sample_rate = goal_sample_rate
+        self.max_iter = max_iter
+        self.obstacle_list = obstacle_list
+        self.node_list = []
         self.connect_circle_dist = connect_circle_dist
         self.goal_node = self.Node(goal[0], goal[1])
         self.search_until_max_iter = search_until_max_iter
@@ -70,8 +80,8 @@ class RRTStar(RRT):
                                   self.expand_dis)
             near_node = self.node_list[nearest_ind]
             new_node.cost = near_node.cost + \
-                math.hypot(new_node.x-near_node.x,
-                           new_node.y-near_node.y)
+                            math.hypot(new_node.x - near_node.x,
+                                       new_node.y - near_node.y)
 
             if self.check_collision(new_node, self.obstacle_list):
                 near_inds = self.find_near_nodes(new_node)
@@ -101,6 +111,69 @@ class RRTStar(RRT):
             return self.generate_final_course(last_index)
 
         return None
+
+    def get_random_node(self):
+        if random.randint(0, 100) > self.goal_sample_rate:
+            rnd = self.Node(
+                random.uniform(self.min_rand, self.max_rand),
+                random.uniform(self.min_rand, self.max_rand))
+        else:  # goal point sampling
+            rnd = self.Node(self.end.x, self.end.y)
+        return rnd
+
+    @staticmethod
+    def get_nearest_node_index(node_list, rnd_node):
+        dlist = [(node.x - rnd_node.x) ** 2 + (node.y - rnd_node.y) ** 2
+                 for node in node_list]
+        minind = dlist.index(min(dlist))
+
+        return minind
+
+    @staticmethod
+    def check_collision(node, obstacleList):
+
+        if node is None:
+            return False
+
+        for (ox, oy, size) in obstacleList:
+            dx_list = [ox - x for x in node.path_x]
+            dy_list = [oy - y for y in node.path_y]
+            d_list = [dx * dx + dy * dy for (dx, dy) in zip(dx_list, dy_list)]
+
+            if min(d_list) <= size ** 2:
+                return False  # collision
+
+        return True  # safe
+
+    def steer(self, from_node, to_node, extend_length=float("inf")):
+
+        new_node = self.Node(from_node.x, from_node.y)
+        d, theta = self.calc_distance_and_angle(new_node, to_node)
+
+        new_node.path_x = [new_node.x]
+        new_node.path_y = [new_node.y]
+
+        if extend_length > d:
+            extend_length = d
+
+        n_expand = math.floor(extend_length / self.path_resolution)
+
+        for _ in range(n_expand):
+            new_node.x += self.path_resolution * math.cos(theta)
+            new_node.y += self.path_resolution * math.sin(theta)
+            new_node.path_x.append(new_node.x)
+            new_node.path_y.append(new_node.y)
+
+        d, _ = self.calc_distance_and_angle(new_node, to_node)
+        if d <= self.path_resolution:
+            new_node.path_x.append(to_node.x)
+            new_node.path_y.append(to_node.y)
+            new_node.x = to_node.x
+            new_node.y = to_node.y
+
+        new_node.parent = from_node
+
+        return new_node
 
     def choose_parent(self, new_node, near_inds):
         """
@@ -187,9 +260,9 @@ class RRTStar(RRT):
         # expand_dist
         if hasattr(self, 'expand_dis'):
             r = min(r, self.expand_dis)
-        dist_list = [(node.x - new_node.x)**2 + (node.y - new_node.y)**2
+        dist_list = [(node.x - new_node.x) ** 2 + (node.y - new_node.y) ** 2
                      for node in self.node_list]
-        near_inds = [dist_list.index(i) for i in dist_list if i <= r**2]
+        near_inds = [dist_list.index(i) for i in dist_list if i <= r ** 2]
         return near_inds
 
     def rewire(self, new_node, near_inds):
@@ -237,6 +310,59 @@ class RRTStar(RRT):
                 node.cost = self.calc_new_cost(parent_node, node)
                 self.propagate_cost_to_leaves(node)
 
+    def generate_final_course(self, goal_ind):
+        path = [[self.end.x, self.end.y]]
+        node = self.node_list[goal_ind]
+        while node.parent is not None:
+            path.append([node.x, node.y])
+            node = node.parent
+        path.append([node.x, node.y])
+
+        return path
+
+    def calc_dist_to_goal(self, x, y):
+        dx = x - self.end.x
+        dy = y - self.end.y
+        return math.hypot(dx, dy)
+
+    def draw_graph(self, rnd=None):
+        plt.clf()
+        # for stopping simulation with the esc key.
+        plt.gcf().canvas.mpl_connect(
+            'key_release_event',
+            lambda event: [exit(0) if event.key == 'escape' else None])
+        if rnd is not None:
+            plt.plot(rnd.x, rnd.y, "^k")
+        for node in self.node_list:
+            if node.parent:
+                plt.plot(node.path_x, node.path_y, "-g")
+
+        for (ox, oy, size) in self.obstacle_list:
+            self.plot_circle(ox, oy, size)
+
+        plt.plot(self.start.x, self.start.y, "xr")
+        plt.plot(self.end.x, self.end.y, "xr")
+        plt.axis("equal")
+        plt.axis([-2, 15, -2, 15])
+        plt.grid(True)
+        plt.pause(0.01)
+
+    @staticmethod
+    def plot_circle(x, y, size, color="-b"):  # pragma: no cover
+        deg = list(range(0, 360, 5))
+        deg.append(0)
+        xl = [x + size * math.cos(np.deg2rad(d)) for d in deg]
+        yl = [y + size * math.sin(np.deg2rad(d)) for d in deg]
+        plt.plot(xl, yl, color)
+
+    @staticmethod
+    def calc_distance_and_angle(from_node, to_node):
+        dx = to_node.x - from_node.x
+        dy = to_node.y - from_node.y
+        d = math.hypot(dx, dy)
+        theta = math.atan2(dy, dx)
+        return d, theta
+
 
 def main():
     print("Start " + __file__)
@@ -269,14 +395,12 @@ def main():
         plt.grid(True)
         plt.show()
     else:
-        print("found path with length of: ",rrt_star.end.cost)
+        print("found path with length of: ", rrt_star.end.cost)
         rrt_star.draw_graph()
         plt.plot([x for (x, y) in path], [y for (x, y) in path], 'r--')
         plt.grid(True)
         plt.show()
         # Draw final path
-
-
 
 
 if __name__ == '__main__':
